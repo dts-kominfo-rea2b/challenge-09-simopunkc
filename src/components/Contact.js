701bb5d27aabb7908f03412f66bb025238769cf3
simopunkc <pembela.allah@gmail.com>
2022-06-18 17:32:36 +0700

    submit challenge 9 oleh budiyanto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div className="profile">
+            <img src={data.photo} alt={data.name} />
+            <div className='contact-info'>
+                <h4><b>{data.name}</b></h4>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+            </div>
+            <div className='clear'></div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <h1>Call a Friend</h1>
+            <h2>your friendly contact app</h2>
+        </>
     )
 }
 
